cd440ced918f18ce9a404a05291583cdcb9011a2
David Wils <david.wils591@gmail.com>
2022-03-03 15:52:40 +0100

    creation objet heroes

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const heros = [
+    {   
+        "image" : "captain_america.jpg",
+        
+        "nom_hero" : "Captain American",
+        "nom_civil" : "steven steve roger",
+        "equipe" : ["Avengers"],
+        "biographie" :"Né en 1922 dans le Lower East Side de Manhattan, dans une famille d’origine irlandaise, Steve Rogers grandit durant la crise de 1929. Adolescent frêle, avec une passion pour les univers fantastiques, Steve était souvent protégé contre les brutes de son quartier par son ami Arnold « Arnie » Roth, et il fut aussi, en une occasion, sauvé de ses agresseurs par le gros bras du quartier Duvid Fortunov. Steve hérita d’un solide sens de la justice et de la compassion de son idéaliste de grand-père, un grand-père qui lui racontait des histoires du Chemin de fer clandestin, ou d’autres combats contre l’oppression. Le père de Steve, souvent au chômage, alcoolique chronique, mourut alors que son fils était encore un enfant, laissant sa mère Sarah seule pour élever leur fils et subvenir à leurs besoins à tous deux. Diplômé jeune du lycée, Steve se retrouva brusquement orphelin quand sa mère mourut de pneumonie. Il poursuivit néanmoins ses rêves de carrière artistique, remportant même, en 1939, un prix, avec la médaille d’or des Arts Créatifs lors du tournoi annuel des « Arts de l’avenir »."
+    },
+    {
+        "image" : "ironman.png",
+        "nom_hero" : "Iron Man",  
+        "nom_civil" : "Tony Stark",
+        "equipe" : ["Avengers"],
+        "biographie" : "Anthony « Tony » Stark, alias Iron Man, est le fils de Howard et Maria Stark, dont la compagnie Stark Industries était l'une des plus importantes corporations US. A l'âge de sept ans, Tony fut envoyé à l'internat par son père dans l'espoir de l'« endurcir ». Tony trouvait difficile à se lier avec des gens, et il devint fasciné par les machines, qui pouvaient être contrôlés ou réparés."
+    },
+    {
+    
+        "image":"incredible_hulk.jpg",
+        "nom_hero":"Hulk",
+        "nom_civil":"Bruce Banner",
+        "equipe":["The Avengers", 
+        "The Defenders"
+        ],
+        "biographie": "Le docteur Bruce Banner, un brillant physicien nucléaire, crée pour les forces armées des États-Unis un nouveau type d'arme nucléaire, la bombe G, basée sur des rayons gamma. Durant un essai, Banner aperçoit un adolescent, Rick Jones, allongé dans sa voiture, qui répond bêtement à un pari de ses camarades. Demandant à son assistant Igor Starsky de stopper le compte à rebours, le physicien court vers la zone d'essai et sauve le jeune inconscient en le poussant dans une tranchée de protection. Mais Starsky, qui en réalité se nomme Drenkov, est un espion soviétique à la solde du gouvernement envoyé pour s'emparer des secrets de Banner. Il laisse la bombe exploser. Le docteur n'a pas le temps de se mettre à l'abri ; il est alors bombardé de rayons gamma, ce qui a pour effet de modifier profondément son ADN mais qui, à la surprise de tous, ne le tue pas.Banner reste en observation à l'infirmerie en compagnie de Rick Jones jusqu'au soir où, dans une intense souffrance, il voit son corps se transformer en un individu aux proportions colossales tandis que sa peau devient grise et que son psychisme se modifie pour laisser place à une intelligence quasi animale. Dans cet état, il détruit le mur de l'infirmerie simplement en le poussant de la main. Peu après, lorsqu'une jeep de l'armée lui fonce dessus, il reste immobile alors que le véhicule s'écrase sur lui sans qu'il subisse de dommages. Cela démontre qu'il possède une force et une résistance stupéfiantes. En raison de sa corpulence, un garde le surnomme « Hulk » lors d'une description faite à ses supérieurs. Depuis, lors de moments de stress ou de colère intense, le docteur Banner se métamorphose en une créature colossale à la peau verte (cette couleur ayant rapidement remplacé celle de sa peau grise originale) dotée d'une force phénoménale et animée par une rage qu'il ne parvient pas à contrôler (cette rage est présente en Banner depuis sa jeunesse quand, petit garçon, il était battu par son père et quand il a vu sa mère mourir sous les coups de ce dernier)."
+    },
+    {
+        "image" : "thor-1-1080x675.jpg",
+    
+        "nom_hero" : "Thor",
+    
+        "nom_civil" : "Thor Odinson",
+    
+        "equipe" : "avengers",
+    
+        "biographie": "Thor est le fils du roi des Asgardiens Odin et de la déesse de la terre Gaea. Il est élevé par sa mère adoptive Frigga, l'épouse d'Odin. Au cours de son enfance, Thor apprend à être un guerrier et grandit en popularité parmi son peuple. Son père fait construire le marteau Mjöllnir pour lui. Lorsqu'il le reçoit, il devient le dieu nordique de la foudre et le plus grand guerrier d'Asgard.Thor a de nombreuses aventures et commence à devenir fier. Il rompt un traité de paix avec les géants des glaces, au risque de déclencher une guerre. Pour punir l'arrogance de son fils, Odin décide que Thor a besoin d'une leçon d'humilité. Il l’envoie sur Terre (nommée Midgard par les dieux scandinaves), efface sa mémoire et l’enferme dans le corps du mortel Donald Blake, un médecin infirme et timide.",
+    },
+
+]
